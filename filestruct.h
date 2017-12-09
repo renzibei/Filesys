692384141db7e832b7d49d94ce780e13987aa647
Fan Qu <fan_qu@icloud.com>
2017-12-09 21:13:01 +0800

    modift filestruct.h

--- filestruct.h
+++ filestruct.h
@@ -1,22 +1,22 @@
 #ifndef FILESTRUCT_H
 #define FILESTRUCT_H
 
-struct super_block {
+struct _super_block {
     bool inode_bitmap[4096];
     bool block_bitmap[4096];
-};
+} super_blocks;
 
-struct inode {
+struct _inode {
     int i_id;
     int i_mode;
     int i_file_size;
     //...
     int i_blocks[1];
     char i_place_holder[16]; //占位符
-};
+} inodes;
 
 //文件对应数据块
-struct file_block {
+struct _file_block {
     char data[4096];
 };
 
