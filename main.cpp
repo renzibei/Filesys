eecac07a16183baaed6de015ffecfdeca0fbbffa
Fan Qu <fan_qu@icloud.com>
2018-01-05 21:17:23 +0800

    zhetengleyifan

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 _super_block sbks;
 _inode inodes[4096];
 FileView *vfs_gui;
-
+QTranslator *tran;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    tran = new QTranslator;
+    bool ok = 0;
+    ok = tran->load("qt_zh_CN.qm",":/trans/");
+    a.installTranslator(tran);
     vfs_gui = new FileView;
     vfs_gui->show();
 
