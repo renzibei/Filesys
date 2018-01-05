960448f4df96d3d56420c883c442f8fc5cc5d778
Fan Qu <fan_qu@icloud.com>
2018-01-05 09:33:03 +0800

    change sth

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "fileview.h"
+#include <QApplication>
+#define _CRT_SECURE_NO_WARNINGS
+
+_super_block sbks;
+_inode inodes[4096];
+FileView *vfs_gui;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    FileView w;
+    w.show();
+
+    return a.exec();
+}
--- qu.h
+++ qu.h
@@ -1,10 +1,10 @@
 #ifndef QU_H
 #define QU_H
 #include "Filesys.h"
-
+/*
 extern const char filename[12];
 extern const int inode_size,datablk_size, dir_size, indbmp_size, blkbmp_size, inodes_size;
-
+*/
 struct workdir_pathnode{
     int dir_inode;
     char dirname[252];
